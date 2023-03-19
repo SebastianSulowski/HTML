ca5d8240e45e1acb74f84e9c3e0207c30f81d1b6
SebastianSulowski <116625989+SebastianSulowski@users.noreply.github.com>
2023-03-19 22:07:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/mypage/me')
 def about_me():
-    return render_template('about_me.html')
+    return render_template('index.html')
 
 @app.route('/mypage/contact', methods=['GET', 'POST'])
 def contact_info():
     if request.method == 'POST':
         message = request.form['message']
         print(f"Received message: {message}")
-    return render_template('contact_info.html')
+    return render_template('index_1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
